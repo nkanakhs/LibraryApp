aa1aad82dcf5ed1b6e47fe1fb0d231038d8b8165
orfeasladop <orf.ladop@gmail.com>
2024-11-06 20:01:58 +0200

    1)customer-image 2)footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { WrapperComponent } from "./wrapper/wrapper.component";
 import { HomepageComponent } from "./homepage/homepage.component";
 import { TopNavComponent } from "./top-nav/top-nav.component";
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WrapperComponent, HomepageComponent, TopNavComponent, MatFormFieldModule],
+  imports: [RouterOutlet, WrapperComponent, HomepageComponent, TopNavComponent, MatFormFieldModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
